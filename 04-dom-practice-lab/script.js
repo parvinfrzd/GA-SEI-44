db6872fd1bd9c353e958bef7b31419743ecafbd7
parvinfrzd <parv93farah@gmail.com>
2021-10-15 18:10:32 -0400

    complete dom element one

--- 04-dom-practice-lab/script.js
+++ 04-dom-practice-lab/script.js
@@ -1,2 +1,27 @@
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
 let mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
+var h1 = document.createElement('h1');
+h1.innerHTML = 'SEI Rocks!';
+mainEl.appendChild(h1);
+mainEl.classList.add('flex-ctr');
+
+var topMenuEl = document.querySelector('#top-menu');
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+Object.values(menuLinks).forEach(function (link) {
+    var a = document.createElement('a');
+    a.innerHTML = link.text;
+    a.setAttribute('href', link.href);
+    topMenuEl.appendChild(a);
+    console.log(topMenuEl);
+});

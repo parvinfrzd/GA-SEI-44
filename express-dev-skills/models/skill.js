be9581de927b6d5621c8939a1a956315132770da
parvinfrzd <parvin.farahzadeh@gmail.com>
2021-11-28 12:21:13 -0500

    project setup

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,13 @@
+const skills = [
+    { id: 125223, skill: 'Full-stack Development', advanced: true },
+    { id: 127904, skill: 'Gameplay Development', advanced: false },
+    { id: 139608, skill: 'AR/VR', advanced: false }
+];
+
+function getAll() {
+    return skills;
+}
+
+module.exports = {
+    getAll
+};

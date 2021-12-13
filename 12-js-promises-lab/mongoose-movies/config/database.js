136a051c014e76d5246400ddb629aa50e4035b6c
Parvin Farahzadeh <parv93farah@gmail.com>
2021-12-13 17:26:39 -0500

    solve js promises  lab

--- 12-js-promises-lab/mongoose-movies/config/database.js
+++ 12-js-promises-lab/mongoose-movies/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies',
-{ useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect('mongodb://127.0.0.1:27017/movies',
+  { useNewUrlParser: true, useCreateIndex: true }
 );
 
 // shortcut to mongoose.connection object
--- 12-js-promises-lab/mongoose-movies/query-practice.js
+++ 12-js-promises-lab/mongoose-movies/query-practice.js
@@ -11,38 +11,61 @@ Promise.resolve().then(function () {
 }).then(function (result) {
     console.log('1): ', result)
     // 2) Find all performer docs
-
+    return Performer.find({});
 }).then(function (result) {
     console.log('2): ', result)
-    // Follow the same .then structure used above from this point forward
-    // Don't forget to console.log the exercise number also as shown above 
+
     // 3) Find all movies with an MPAA Rating of 'PG'
+    return Movie.find({ mpaaRating: "PG" }).exec();
+}).then(function (result) {
+    console.log('3): ', result)
+
+    // 4) Find all movies that are still showing
+    return Movie.find({ nowShowing: true }).exec();
+}).then(function (result) {
+    console.log('4): ', result)
+
+    // 5) Find all movies with an MPAA Rating of 'PG' or 'PG-13'
+    return Movie.where('mpaaRating').in(['PG-13', 'PG']);
+}).then(function (result) {
+    console.log('5): ', result)
+
+    // 6) Find the first movie found with a releaseYear of 2018
+    return Movie.findOne({ releaseYear: 2018 })
+}).then(function (result) {
+    console.log('6): ', result)
+
+    // 7) Find all movies released after 1980
+    return Movie.where('releaseYear').gt('1980');
+}).then(function (result) {
+    console.log('7): ', result)
+
+    // 8) Find all movies whose titles start with a 'C'
+    return Movie.find({ title: /^C/ });
+}).then(function (result) {
+    console.log('8): ', result)
+
+    // 9) Find the performer named 'Rami Malek'
+    return Performer.find({ name: 'Rami Malek' })
+}).then(function (result) {
+    console.log('9): ', result)
+
+    // 10) Find all performers born before 1980
+    return Performer.where('born').lt('1980')
+    // Shouldn't we use Dates? In the 
+
+}).then(function (result) {
+    console.log('10): ', result)
+
+    // 11) Find all performers whose name starts with a 'J'
+    return Performer.find({ name: /^J/ });
+
+}).then(function (result) {
+    console.log('11): ', result)
 
 })
 
-    // 4) Find all movies that are still showing
 
-
-    // 5) Find all movies with an MPAA Rating of 'PG' or 'PG-13'
-
-
-    // 6) Find the first movie found with a releaseYear of 2018
-
-
-    // 7) Find all movies released after 1980
-
-
-    // 8) Find all movies whose titles start with a 'C'
-    // Hint: StackOverflow will show how to use a regular expression
-
-
-    // 9) Find the performer named 'Rami Malek'
-
-
-    // 10) Find all performers born before 1980
-
-
-    // 11) Find all performers whose name starts with a 'J'
 
 
     // 12) Add a reference to performer 'Bill Murray' to

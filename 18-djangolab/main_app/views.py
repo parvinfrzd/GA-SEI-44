c87d61e0625c8e7de781781ae3c750e11aaa1752
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-01 20:15:44 -0500

    add partials and about ui

--- 18-djangolab/main_app/views.py
+++ 18-djangolab/main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request): 
     return HttpResponse('YO YO PARVIN')
+
+def about(request): 
+    return render(request, 'main_app/about.html')
